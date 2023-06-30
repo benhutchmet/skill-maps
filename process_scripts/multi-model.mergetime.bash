d2f26c63b269409fddde72410ed45bfb042cc6a4
benhutchmet <b.hutchins@pgr.reading.ac.uk>
2023-06-30 10:13:11 +0100

    changes to input and output files for mergetime

--- process_scripts/multi-model.mergetime.bash
+++ process_scripts/multi-model.mergetime.bash
@@ -30,22 +30,27 @@ init_scheme=$7
 # make sure that cdo is loaded
 module load jaspy
 
-# set up the files
-files=/work/scratch-nopw/benhutch/$variable/$model/$region/years_${forecast_range}/$season/outputs/mean-years-${forecast_range}-${season}-${region}-${variable}_Amon_${model}_dcppA-hindcast_s????-r${run}i${init_scheme}*.nc
+# anoms directory from which to extract the files
+base_dir="/work/scratch-nopw/benhutch/$variable/$model/$region/years_${forecast_range}/$season/outputs/anoms"
 
+# file pattern of the anoms files
+files_pattern="mean-years-${forecast_range}-${season}-${region}-${variable}_Amon_${model}_dcppA-hindcast_s????-r${run}i${init_scheme}*-anoms.nc"
+
+# set up the files
+# combine the base directory and the file pattern
+files="${base_dir}/${files_pattern}"
 
 # echo the files to be merged
 echo "Files to be merged: $files"
 
 # set the output directory
 # send to the home directory
-OUTPUT_DIR=/home/users/benhutch/skill-maps-processed-data
-/$variable/$model/$region/years_${forecast_range}/$season/outputs/mergetime
+OUTPUT_DIR="/home/users/benhutch/skill-maps-processed-data/${variable}/${model}/${region}/years_${forecast_range}/${season}/outputs/mergetime"
 mkdir -p $OUTPUT_DIR
 
 # set the output file
 mergetime_fname="mergetime_${model}_${variable}_${region}_${forecast_range}_${season}-r${run}i${init_scheme}.nc"
-OUTPUT_FILE=$OUTPUT_DIR/$mergetime_fname
+OUTPUT_FILE=${OUTPUT_DIR}/${mergetime_fname}
 
 # echo the output file
 echo "Output file: $OUTPUT_FILE"
