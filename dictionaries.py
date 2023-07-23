78d52b65471d6d78ba4447fcfd651deda0f53399
benhutchmet <b.hutchins@pgr.reading.ac.uk>
2023-07-23 15:39:17 +0100

    need to actually process norcpm data before plotting

--- dictionaries.py
+++ dictionaries.py
@@ -11,6 +11,7 @@ gif_plots_dir = base_dir + "/plots/gif"
 # list of the test model
 test_model = [ "CMCC-CM2-SR5" ]
 test_model2 = [ "EC-Earth3" ]
+test_model_norcpm = [ "NorCPM1" ]
 
 # List of the full models
 models = [ "BCC-CSM2-MR", "MPI-ESM1-2-HR", "CanESM5", "CMCC-CM2-SR5", "HadGEM3-GC31-MM", "EC-Earth3", "MPI-ESM1-2-LR", "FGOALS-f3-L", "MIROC6", "IPSL-CM6A-LR", "CESM1-1-CAM5-CMIP5", "NorCPM1" ]
