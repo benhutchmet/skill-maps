075fb911d1b7337f84c37ada5fff4722d75909a0
benhutchmet <b.hutchins@pgr.reading.ac.uk>
2023-07-23 16:45:15 +0100

    add a textbox which includes the model name

--- python/functions.py
+++ python/functions.py
@@ -1270,6 +1270,9 @@ def plot_spatial_correlations(models, obs, variable_data, region, season, foreca
         # Add title
         ax.set_title(f"{model} {variable} {region} {season} {forecast_range} Correlation Coefficients")
 
+        # Add textbox with model name
+        ax.text(0.05, 0.95, model, transform=ax.transAxes, fontsize=14, fontweight='bold', va='top')
+
     # set up the path for saving the figure
     fig_name = f"{variable}_{region}_{season}_{forecast_range}_correlation_coefficients_{datetime.now().strftime('%Y%m%d_%H%M%S')}.png"
     fig_path = os.path.join(plots_dir, fig_name)
