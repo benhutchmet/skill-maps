b171ceca2e25a5a123cf8f46be482f9c13efc31f
benhutchmet <b.hutchins@pgr.reading.ac.uk>
2023-07-26 11:33:33 +0100

    change the size

--- python/functions.py
+++ python/functions.py
@@ -1371,7 +1371,7 @@ def plot_correlations_subplots(models, obs, variable_data, variable, region, sea
     proj = ccrs.PlateCarree()
     
     # Set the figure size and subplot parameters
-    fig, axs = plt.subplots(nrows=4, ncols=3, figsize=(18, 20), subplot_kw={'projection': proj}, gridspec_kw={'hspace': 0.05, 'wspace': 0.1})
+    fig, axs = plt.subplots(nrows=4, ncols=3, figsize=(18, 18), subplot_kw={'projection': proj}, gridspec_kw={'wspace': 0.1})
 
     # Flatten the axs array
     axs = axs.flatten()
@@ -1466,8 +1466,8 @@ def plot_correlations_subplots(models, obs, variable_data, variable, region, sea
     fig_name = f"{variable}_{region}_{season}_{forecast_range}_correlation_coefficients_{datetime.now().strftime('%Y%m%d_%H%M%S')}.png"
     fig_path = os.path.join(plots_dir, fig_name)
 
-    # Adjust the vertical spacing between the plots
-    plt.subplots_adjust(hspace=0.1)
+    # # Adjust the vertical spacing between the plots
+    # plt.subplots_adjust(hspace=0.1)
 
     # Save the figure
     plt.savefig(fig_path, dpi=300, bbox_inches='tight')
