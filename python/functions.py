ab8a1d307e21e3ef3043e7766da58abfbc09d82d
benhutchmet <b.hutchins@pgr.reading.ac.uk>
2023-07-26 11:06:25 +0100

    change dimesnsions of subplots

--- python/functions.py
+++ python/functions.py
@@ -1371,8 +1371,8 @@ def plot_correlations_subplots(models, obs, variable_data, variable, region, sea
     proj = ccrs.PlateCarree()
     
     # Set the figure size and subplot parameters
-    fig, axs = plt.subplots(nrows=4, ncols=3, figsize=(10, 20), subplot_kw={'projection': proj})
-
+    fig, axs = plt.subplots(nrows=4, ncols=3, figsize=(18, 18), subplot_kw={'projection': proj})
+    
     # Flatten the axs array
     axs = axs.flatten()
     
