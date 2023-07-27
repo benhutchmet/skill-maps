84eff265567a90ac2f5bb1c0d2ed904d7701da21
benhutchmet <b.hutchins@pgr.reading.ac.uk>
2023-07-27 11:28:20 +0100

    moved back out of function

--- python/functions.py
+++ python/functions.py
@@ -1392,6 +1392,30 @@ def plot_correlations_subplots(models, obs, variable_data, variable, region, sea
     # Set the projection
     proj = ccrs.PlateCarree()
     
+    # Set up the lats and lons for the azores grid
+    azores_lon1, azores_lon2 = azores_grid['lon1'], azores_grid['lon2']
+    azores_lat1, azores_lat2 = azores_grid['lat1'], azores_grid['lat2']
+
+    # Set up the lats and lons for the iceland grid
+    iceland_lon1, iceland_lon2 = iceland_grid['lon1'], iceland_grid['lon2']
+    iceland_lat1, iceland_lat2 = iceland_grid['lat1'], iceland_grid['lat2']
+    
+    # Set up the lats and lons for the northern UK index box
+    uk_n_lon1, uk_n_lon2 = uk_n_box['lon1'], uk_n_box['lon2']
+    uk_n_lat1, uk_n_lat2 = uk_n_box['lat1'], uk_n_box['lat2']
+
+    # Set up the lats and lons for the southern UK index box
+    uk_s_lon1, uk_s_lon2 = uk_s_box['lon1'], uk_s_box['lon2']
+    uk_s_lat1, uk_s_lat2 = uk_s_box['lat1'], uk_s_box['lat2']
+
+    # subtract 180 from all of the azores and iceland lons
+    azores_lon1, azores_lon2 = azores_lon1 - 180, azores_lon2 - 180
+    iceland_lon1, iceland_lon2 = iceland_lon1 - 180, iceland_lon2 - 180
+
+    # subtract 180 from all of the uk lons
+    uk_n_lon1, uk_n_lon2 = uk_n_lon1 - 180, uk_n_lon2 - 180
+    uk_s_lon1, uk_s_lon2 = uk_s_lon1 - 180, uk_s_lon2 - 180
+
     # Set the figure size and subplot parameters
     fig, axs = plt.subplots(nrows=4, ncols=3, figsize=(18, 16), subplot_kw={'projection': proj}, gridspec_kw={'wspace': 0.1})
 
@@ -1412,30 +1436,6 @@ def plot_correlations_subplots(models, obs, variable_data, variable, region, sea
     
         # Calculate the spatial correlations for the model
         rfield, pfield, obs_lons_converted, lons_converted = calculate_spatial_correlations(obs, variable_data, model)
-
-        # Set up the lats and lons for the azores grid
-        azores_lon1, azores_lon2 = azores_grid['lon1'], azores_grid['lon2']
-        azores_lat1, azores_lat2 = azores_grid['lat1'], azores_grid['lat2']
-
-        # Set up the lats and lons for the iceland grid
-        iceland_lon1, iceland_lon2 = iceland_grid['lon1'], iceland_grid['lon2']
-        iceland_lat1, iceland_lat2 = iceland_grid['lat1'], iceland_grid['lat2']
-        
-        # Set up the lats and lons for the northern UK index box
-        uk_n_lon1, uk_n_lon2 = uk_n_box['lon1'], uk_n_box['lon2']
-        uk_n_lat1, uk_n_lat2 = uk_n_box['lat1'], uk_n_box['lat2']
-
-        # Set up the lats and lons for the southern UK index box
-        uk_s_lon1, uk_s_lon2 = uk_s_box['lon1'], uk_s_box['lon2']
-        uk_s_lat1, uk_s_lat2 = uk_s_box['lat1'], uk_s_box['lat2']
-
-        # subtract 180 from all of the azores and iceland lons
-        azores_lon1, azores_lon2 = azores_lon1 - 180, azores_lon2 - 180
-        iceland_lon1, iceland_lon2 = iceland_lon1 - 180, iceland_lon2 - 180
-
-        # subtract 180 from all of the uk lons
-        uk_n_lon1, uk_n_lon2 = uk_n_lon1 - 180, uk_n_lon2 - 180
-        uk_s_lon1, uk_s_lon2 = uk_s_lon1 - 180, uk_s_lon2 - 180
 
         # Set up the converted lons
         lons_converted = lons_converted - 180
