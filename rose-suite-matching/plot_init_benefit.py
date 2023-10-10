63400bdfb6114153da0ed38e3c9a429ddb33250e
benhutchmet <b.hutchins@pgr.reading.ac.uk>
2023-10-10 14:07:34 +0100

    new function for extracting arrays

--- rose-suite-matching/plot_init_benefit.py
+++ rose-suite-matching/plot_init_benefit.py
@@ -95,10 +95,10 @@ def extract_values_from_txt(path, variable):
     print(start_end_file)
 
     # Load the values from the files
-    values['nens1'] = (np.loadtxt(nens1_file))[0].astype(int)
-    values['nens2'] = (np.loadtxt(nens2_file))[0].astype(int)
-    values['sigo'] = (np.loadtxt(sigo_file))[0]
-    values['sigo_resid'] = (np.loadtxt(sigo_resid_file))[0]
+    values['nens1'] = np.loadtxt(nens1_file).astype(int)
+    values['nens2'] = np.loadtxt(nens2_file).astype(int)
+    values['sigo'] = np.loadtxt(sigo_file)
+    values['sigo_resid'] = np.loadtxt(sigo_resid_file)
     values['start_year'] = (np.loadtxt(start_end_file))[0].astype(int)
     values['end_year'] = (np.loadtxt(start_end_file))[1].astype(int)
 
@@ -106,5 +106,68 @@ def extract_values_from_txt(path, variable):
     return values
 
 # Now define a function to extract the numpy arrays within the path
+def load_arrays_from_npy(path, variable):
+    """
+    Loads the numpy arrays from a path into a dictionary.
+    
+    Args:
+        path (str): path to the numpy arrays
+        variable (str): variable to extract values for
+        
+    Returns:
+        arrays (dict): dictionary of numpy arrays
+            Contains:
+                - 'corr1' (np.ndarray): correlation of the first ensemble
+                                        (initialized) with the observations
+                - 'corr1_p' (np.ndarray): p-value of the correlation of the
+                                            first ensemble (initialized) with
+                                            the observations
+                - 'partial_r' (np.ndarray): the bias corrected correlation of 
+                                            the first ensemble (initialized)
+                                            with the observations (after 
+                                            accounting for uninitialized trend)
+                - 'partial_r_p' (np.ndarray): p-value of the bias corrected
+                                                correlation of the first 
+                                                ensemble (initialized) with
+                                                the observations (after 
+                                                accounting for uninitialized
+                                                trend)
+    
+    """
 
+    # Set up the dictionary
+    arrays = {
+        'corr1': None,
+        'corr1_p': None,
+        'partial_r': None,
+        'partial_r_p': None
+    }
+
+    # extract the .npy files using glob
+    files = glob.glob(f'{path}/*.npy')
+
+    # Print the files
+    print(files)
+
+    # find the file containing corr1
+    corr1_file = [file for file in files if f'corr1_{variable}' in file]
+
+    # find the file containing corr1_p
+    corr1_p_file = [file for file in files if 'corr1_p' in file]
+
+    # find the file containing partial_r
+    partial_r_file = [file for file in files if f'partial_r_{variable}' 
+                      in file]
+
+    # find the file containing partial_r_p
+    partial_r_p_file = [file for file in files if 'partial_r_p' in file]
+
+    # Load the arrays from the files
+    arrays['corr1'] = np.load(corr1_file)
+    arrays['corr1_p'] = np.load(corr1_p_file)
+    arrays['partial_r'] = np.load(partial_r_file)
+    arrays['partial_r_p'] = np.load(partial_r_p_file)
+
+    # Return the arrays
+    return arrays
 
