3b5aa803c80ce37c13f8d31dae185b6538c3cb13
benhutchmet <b.hutchins@pgr.reading.ac.uk>
2023-10-10 13:46:47 +0100

    adding loading file function

--- rose-suite-matching/plot_init_benefit.py
+++ rose-suite-matching/plot_init_benefit.py
@@ -25,4 +25,84 @@ Usage:
                                 
 """
 
-#
+# Import general Python modules
+import argparse, os, sys, glob, re
+
+# Import additional modules
+import numpy as np
+
+# Define a function to extract the values from the txt file
+def extract_values_from_txt(path, variable):
+    """
+    Extract values from a txt file.
+
+    Args:
+        path (str): path to the txt file
+        variable (str): variable to extract values for
+
+    Returns:
+        values (dict): dictionary of values
+            Contains:
+                - 'nens1' (int): number of ensemble members in the first 
+                                    ensemble
+                - 'nens2' (int): number of ensemble members in the second
+                                    ensemble
+                - 'sigo' (float): standard deviation of the observations
+                - 'sigo_resid' (float): standard deviation of the observed
+                                            residuals
+                - 'start_year' (int): start year of the forecast
+                - 'end_year' (int): end year of the forecast
+    
+    """
+
+    # Set up the mdi
+    mdi = -9999.0
+
+    # Set up the dictionary
+    values = {
+        'nens1': mdi,
+        'nens2': mdi,
+        'sigo': mdi,
+        'sigo_resid': mdi,
+        'start_year': mdi,
+        'end_year': mdi
+    }
+
+    # Extract the values from the txt file
+    files = glob.glob(path)
+
+    # Print the files
+    print(files)
+
+    # find the file containing nens1
+    nens1_file = [file for file in files if 'nens1' in file][0]
+    print(nens1_file)
+
+    # find the file containing nens2
+    nens2_file = [file for file in files if 'nens2' in file][0]
+    print(nens2_file)
+
+    # find the file containing sigo
+    sigo_file = [file for file in files if f'sigo_{variable}' in file][0]
+    print(sigo_file)
+
+    # find the file containing sigo_resid
+    sigo_resid_file = [file for file in files if 'sigo_resid' in file][0]
+    print(sigo_resid_file)
+
+    # find the file containing start_year
+    start_end_file = [file for file in files if 'start_end_years' in file][0]
+    print(start_end_file)
+
+    # Load the values from the files
+    values['nens1'] = (np.loadtxt(nens1_file)).astype(int)
+    values['nens2'] = (np.loadtxt(nens2_file)).astype(int)
+    values['sigo'] = np.loadtxt(sigo_file)
+    values['sigo_resid'] = np.loadtxt(sigo_resid_file)
+    values['start_year'] = ((np.loadtxt(start_end_file))[0]).astype(int)
+    values['end_year'] = ((np.loadtxt(start_end_file))[1]).astype(int)
+
+    # Return the values
+    return values
+
+
