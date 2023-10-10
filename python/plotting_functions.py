98d25584c1ac8ad9a57cf87ff20c8f1903cc0db2
benhutchmet <b.hutchins@pgr.reading.ac.uk>
2023-10-10 13:29:11 +0100

    testing plotting function

---
+++ python/plotting_functions.py
@@ -0,0 +1,11 @@
+"""
+Functions to be used for creating plots from bootstrapped data.
+"""
+
+# Import general Python modules
+import argparse, os, sys, glob, re
+
+# Import additional modules
+import numpy as np
+import matplotlib.pyplot as plt
+import xarray as xr
--- rose-suite-matching/plot_init_benefit.py
+++ rose-suite-matching/plot_init_benefit.py
@@ -10,4 +10,19 @@ the benefit of the initialization relative to the uninitialized ensemble mean.
 
 Usage:
 
-    $ python plot_init_benefit.py"""
+    $ python plot_init_benefit.py <variable> <region> <season> <forecast_range>
+                                <method> <no_bootstraps>
+    Args:                            
+        <variable>      :   variable to plot
+        <region>        :   region to plot
+        <season>        :   season to plot
+        <forecast_range>:   forecast range to plot
+        <method>        :   method to plot
+        <no_bootstraps> :   number of bootstraps to plot
+
+    Returns:
+        None
+                                
+"""
+
+#
