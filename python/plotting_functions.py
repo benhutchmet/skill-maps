04016b491737bcae57f471cc4df7d46e6913c01b
benhutchmet <b.hutchins@pgr.reading.ac.uk>
2023-10-11 16:04:48 +0100

    extend = both in the right place

--- python/plotting_functions.py
+++ python/plotting_functions.py
@@ -88,7 +88,8 @@ def plot_raw_init_impact(corr1, corr1_p, init_impact, r_partial_p,
 
     # Plot the correlation between the initialized forecast 
     # and the observations
-    cf1 = ax1.contourf(lons, lats, corr1, clevs, cmap='RdBu_r', transform=proj)
+    cf1 = ax1.contourf(lons, lats, corr1, clevs, cmap='RdBu_r', transform=proj,
+                        extend='both')
 
     # If any corr1 values are NaN
     if np.isnan(corr1).any():
@@ -151,7 +152,7 @@ def plot_raw_init_impact(corr1, corr1_p, init_impact, r_partial_p,
 
     # Add a colorbar for the correlation
     cbar = fig.colorbar(cf1, ax=axs, orientation='horizontal', pad = 0.05,
-                        aspect=50, shrink=0.8, extend='both')
+                        aspect=50, shrink=0.8)
     cbar.set_label('correlation coefficient')
 
     # Set up the pathname for saving the figure
