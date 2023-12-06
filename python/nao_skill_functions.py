65657463306c57d67839d2caac9cc420a303cfee
benhutchmet <b.hutchins@pgr.reading.ac.uk>
2023-12-06 12:33:52 +0000

    multi-model mean plot

--- python/nao_skill_functions.py
+++ python/nao_skill_functions.py
@@ -1261,22 +1261,102 @@ def plot_multi_model_mean(nao_stats_dict: dict,
         ax2.fill_between(nao_stats_model['years_short'] - 5, nao_mean_short_min / 100,
                          nao_mean_short_max / 100, color='red', alpha=0.2)
 
-        # Set the title with the ACC and RPC scores
-        ax1.set_title(f"ACC = {corr1:.2f} (p = {p1:.2f}), "
-                      f"RPC = {rpc1:.2f}, "
-                      f"N = {total_nens}")
+    elif lag_and_var_adjust is True:
+        print("Plotting the lag and variance adjusted NAO index")
 
-        # Set the title with the ACC and RPC scores
-        ax2.set_title(f"ACC = {corr1_short:.2f} (p = {p1_short:.2f}), "
-                      f"RPC = {rpc1_short:.2f}, "
-                      f"N = {total_nens}")
-        
-        # Format the initialisation year range in the top left of the figure
-        ax1.text(0.05, 0.95, f"1961-2005", transform=ax1.transAxes, ha='left', va='top',
-                 bbox=dict(facecolor='white', alpha=0.5), fontsize=10)
-        
-        # Format the initialisation year range in the top left of the figure
-        ax2.text(0.05, 0.95, f"1961-2014", transform=ax2.transAxes, ha='left', va='top',
-                 bbox=dict(facecolor='white', alpha=0.5), fontsize=10)
+        # count the number of ensemble members
+        total_nens = lagged_nao_members.shape[0]
 
+        # Calculate the ensemble mean
+        nao_mean = np.mean(lagged_nao_members, axis=0)
+
+        # Calculate the ensemble mean for the short period
+        nao_mean_short = np.mean(lagged_nao_members_short, axis=0)
+
+        # Calculate the correlation between the model NAO index and the observed NAO index
+        corr1, p1 = pearsonr(nao_mean,
+                            nao_stats_dict['BCC-CSM2-MR']['obs_nao_ts_lag'])
+        
+        # Calculate the correlation between the model NAO index and the observed NAO index
+        corr1_short, p1_short = pearsonr(nao_mean_short,
+                                        nao_stats_dict['BCC-CSM2-MR']['obs_nao_ts_lag_short'])
+        
+        # Calculate the RPC between the model NAO index and the observed NAO index
+        rpc1 = corr1 / (np.std(nao_mean) / np.std(lagged_nao_members))
+
+        # Calculate the RPC between the model NAO index and the observed NAO index
+        rpc1_short = corr1_short / (np.std(nao_mean_short) / np.std(lagged_nao_members_short))
+
+        # Calculate the 5th and 95th percentiles
+        nao_mean_min = np.percentile(lagged_nao_members, 5, axis=0)
+        nao_mean_max = np.percentile(lagged_nao_members, 95, axis=0)
+
+        # Calculate the RMSE between the ensemble mean and observations
+        rmse = np.sqrt(np.mean((nao_mean
+                                - nao_stats_dict['BCC-CSM2-MR']['obs_nao_ts_lag'])**2))
+        
+        # Calculate the rmse for the short period
+        rmse_short = np.sqrt(np.mean((nao_mean_short
+                                - nao_stats_dict['BCC-CSM2-MR']['obs_nao_ts_lag_short'])**2))
+        
+        # Calculate the upper and lower confidence intervals
+        ci_lower = nao_mean - (rmse) ; ci_upper = nao_mean + (rmse)
+
+        # Calculate the upper and lower confidence intervals
+        ci_lower_short = nao_mean_short - (rmse_short) ; ci_upper_short = nao_mean_short + (rmse_short)
+
+        # Plot the ensemble mean
+        ax1.plot(nao_stats_model['years_lag'] - 5, nao_mean / 100,
+                 color='red', label='dcppA')
+        
+        # Plot the observed NAO index - time valid for BCC-CSM2-MR
+        model = "BCC-CSM2-MR"
+
+        # Extract the NAO stats for this model
+        ax1.plot(nao_stats_dict[model]['years_lag'] - 5, nao_stats_dict[model]['obs_nao_ts_lag'] / 100,
+                 color='black', label='ERA5')
+        
+        # Plot the 5th and 95th percentiles
+        ax1.fill_between(nao_stats_model['years_lag'] - 5, ci_lower / 100,
+                         ci_upper / 100, color='red', alpha=0.2)
+        
+        # Plot the ensemble mean
+        ax2.plot(nao_stats_model['years_lag_short'] - 5, nao_mean_short / 100,
+                 color='red', label='dcppA')
+        
+        # Plot the observed NAO index - time valid for BCC-CSM2-MR
+        ax2.plot(nao_stats_dict[model]['years_lag_short'] - 5,
+                nao_stats_dict[model]['obs_nao_ts_lag_short'] / 100,
+                    color='black', label='ERA5')
+        
+        # Plot the 5th and 95th percentiles
+        ax2.fill_between(nao_stats_model['years_lag_short'] - 5, ci_lower_short / 100,
+                         ci_upper_short / 100, color='red', alpha=0.2)
+
+    else:
+        raise ValueError("The boolean flags are not set up correctly")
+    
+        # Set the title with the ACC and RPC scores
+    ax1.set_title(f"ACC = {corr1:.2f} (p = {p1:.2f}), "
+                    f"RPC = {rpc1:.2f}, "
+                    f"N = {total_nens}")
+
+    # Set the title with the ACC and RPC scores
+    ax2.set_title(f"ACC = {corr1_short:.2f} (p = {p1_short:.2f}), "
+                    f"RPC = {rpc1_short:.2f}, "
+                    f"N = {total_nens}")
+    
+    # Format the initialisation year range in the top left of the figure
+    ax1.text(0.05, 0.95, "1961-2005", transform=ax1.transAxes, ha='left', va='top',
+                bbox=dict(facecolor='white', alpha=0.5), fontsize=10)
+    
+    # Format the initialisation year range in the top left of the figure
+    ax2.text(0.05, 0.95, "1961-2014", transform=ax2.transAxes, ha='left', va='top',
+                bbox=dict(facecolor='white', alpha=0.5), fontsize=10)
+
+    # Add the legend in the bottom right corner
+    ax1.legend(loc='lower right')
+
+    # Add the legend in the bottom right corner
+    ax2.legend(loc='lower right')
 
