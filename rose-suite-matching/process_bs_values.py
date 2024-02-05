c735107697253e9cfea983d534fe0e6c0529f9c9
benhutchmet <b.hutchins@pgr.reading.ac.uk>
2024-02-05 16:08:01 +0000

    exit once completed

--- rose-suite-matching/process_bs_values.py
+++ rose-suite-matching/process_bs_values.py
@@ -875,6 +875,14 @@ def main():
 
             np.save(save_path + obs_ts_name, forecast_stat["o_ts"])
 
+            # Print that the arrays have been saved
+            print("Arrays saved to:", save_path)
+            print("For alternate lag:", alt_lag_first_year, alt_lag_last_year)
+            print("Finished saving arrays")
+
+            # Exit here
+            sys.exit()
+
         # TODO: also save the output for the NAO time series
     else:
         # If full_period is True, process the raw data
