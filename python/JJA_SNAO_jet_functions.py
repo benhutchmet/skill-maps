3e6bef81c644dc0f8e77a71ca898dce231ae9b69
benhutchmet <b.hutchins@pgr.reading.ac.uk>
2024-02-15 10:09:18 +0000

    function for calculating swj index

--- python/JJA_SNAO_jet_functions.py
+++ python/JJA_SNAO_jet_functions.py
@@ -85,6 +85,77 @@ def calculate_climatology(
 
     return climatology
 
+# Write a function for diagnosing the SWJ index
+def diagnose_swj_index(data_path: str,
+                       season: str = 'JJA',
+                       variable: str = 'u',
+                       lon1: float = 320.0,
+                       lon2: float = 340.0,
+                       lat1: float = 90.0,
+                       lat2: float = 0.0,
+                       level: int = 300):
+    """
+    Diagnoses the Summer Westerly Jet (SWJ) position index for a specified variable, level and region. This SWJ position index
+    is defined as the latitude of peak magnitudes of 300-hPa zonal
+    winds averaged over 320°E–340°E.
+    
+    Parameters:
+    ------------
+    
+    data_path: str
+        Path to the data.
+        
+    variable: str
+        Variable to calculate the SWJ index for.
+        
+    lon1: float
+        Western boundary of the region to calculate the SWJ index for.
+        
+    lon2: float
+        Eastern boundary of the region to calculate the SWJ index for.
+        
+    level: int
+        Level to calculate the SWJ index for. (Mb)
+        
+    Returns:
+    ------------
+    
+    swj_index: xarray.DataArray
+        SWJ index for the specified variable, level and region.
+    """
+
+    # Load the data
+    data = xr.open_dataset(data_path, chunks={'time': 100, 'lat': 100, 'lon': 100})
+
+    # Assert that the months are in the data
+    seasons = {
+        'DJF': [12, 1, 2],
+        'MAM': [3, 4, 5],
+        'JJA': [6, 7, 8],
+        'SON': [9, 10, 11]
+    }
+
+    # Select the season
+    months = seasons[season]
+
+    # Assert that only the specified months are in the data
+    assert all(month in data['time.month'].values for month in months), 'The specified months are not in the data.'
+
+    # Select the level
+    data = data.sel(level=level)
+
+    # Select the region
+    data = data.sel(latitude=slice(lat1, lat2), longitude=slice(lon1, lon2))
+
+    # Calculate the annual mean
+    data = data.groupby('time.year').mean(dim='time')
+
+    # Calculate the SWJ index
+    # Find the latitude of peak magnitudes of 300-hPa zonal winds averaged over 320°E–340°E
+    swj_index = data.mean(dim='lon').max(dim='lat')
+
+    return swj_index
+
 
 
 
